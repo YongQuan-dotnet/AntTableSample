b938f3f7404c70532fb8adc04739660d72cad581
James <james.rao.au@gmail.com>
2022-09-11 21:55:17 +1000

    Add AntDesign Table

---
+++ AntTableSample.Portal/Pages/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿using AntDesign.TableModels;
+using AntTableSample.Portal.DataContext;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntTableSample.Portal.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        IDbContextFactory<PropertyDbContext> DbContextFactory { get; set; }
+
+        private IQueryable<Property> Properties { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var context = DbContextFactory.CreateDbContext();
+            Properties = context.Properties;
+        }
+    }
+}
